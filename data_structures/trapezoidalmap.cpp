62b01ee5ce9e90b52d6b5e285ef924b87bdb8cf2
RobertoAngeloZedda <xrhayux@gmail.com>
2022-05-19 00:15:12 +0200

    class TrapezoidalMap updated

--- data_structures/trapezoidalmap.cpp
+++ data_structures/trapezoidalmap.cpp
@@ -2,6 +2,7 @@
 
 TrapezoidalMap::TrapezoidalMap() {
     trapezoids = std::vector<Trapezoid>();
+
 }
 
 TrapezoidalMap::~TrapezoidalMap() {
@@ -13,14 +14,14 @@ size_t TrapezoidalMap::addTrapezoid(const Trapezoid& trapezoid) {
     return trapezoids.size();
 }
 
-void TrapezoidalMap::updateTrapezoid(const int index, const Trapezoid& trapezoid) {
+void TrapezoidalMap::updateTrapezoid(const size_t index, const Trapezoid& trapezoid) {
     trapezoids[index].~Trapezoid();
     trapezoids[index] = trapezoid;
 }
 
-void TrapezoidalMap::split4(const int index, cg3::Segment2d& segment) {
+void TrapezoidalMap::split4(const size_t trpzToReplace, const cg3::Segment2d& segment) {
     /* Reference of the Trapezoid that needs to be splitted */
-    Trapezoid origin = trapezoids[index];
+    Trapezoid origin = trapezoids[trpzToReplace];
 
     /* Creation of the new 4 Trapezoids */
     Trapezoid t1 = Trapezoid(origin.getTop(), origin.getBot(), origin.getLeftP(), segment.p1());
@@ -30,7 +31,7 @@ void TrapezoidalMap::split4(const int index, cg3::Segment2d& segment) {
 
     /* Indexes in the Trapezoids Vector of the new 4 Trapezoids.
      * The first one will replace the one that is being replaced */
-    size_t t1Index = index;
+    size_t t1Index = trpzToReplace;
     size_t t2Index = addTrapezoid(t2);
     size_t t3Index = addTrapezoid(t3);
     size_t t4Index = addTrapezoid(t4);
@@ -44,12 +45,8 @@ void TrapezoidalMap::split4(const int index, cg3::Segment2d& segment) {
     /* Updating t2's neighbors */
     t2.setTopLeftNeighbor(t1Index);
     t2.setTopRightNeighbor(t4Index);
-    t2.setBotLeftNeighbor(0);
-    t2.setBotRightNeighbor(0);
 
     /* Updating t3's neighbors */
-    t3.setTopLeftNeighbor(0);
-    t3.setTopRightNeighbor(0);
     t3.setBotLeftNeighbor(t1Index);
     t3.setBotRightNeighbor(t4Index);
 
@@ -60,12 +57,12 @@ void TrapezoidalMap::split4(const int index, cg3::Segment2d& segment) {
     t4.setBotRightNeighbor(origin.getBotRightNeighbor());
 
     /* Updating */
-    updateTrapezoid(index, t1);
+    updateTrapezoid(trpzToReplace, t1);
 }
 
-void TrapezoidalMap::split3Left(const int index, cg3::Segment2d& segment) {
+void TrapezoidalMap::split3Left(const size_t trpzToReplace, const cg3::Segment2d& segment) {
     /* Reference of the Trapezoid that needs to be splitted */
-    Trapezoid origin = trapezoids[index];
+    Trapezoid origin = trapezoids[trpzToReplace];
 
     /* Creation of the new 3 Trapezoids */
     Trapezoid t1 = Trapezoid(origin.getTop(), origin.getBot(), origin.getLeftP(), segment.p1());
@@ -73,8 +70,8 @@ void TrapezoidalMap::split3Left(const int index, cg3::Segment2d& segment) {
     Trapezoid t3 = Trapezoid(segment, origin.getBot(), segment.p1(), origin.getRightP());
 
     /* Indexes in the Trapezoids Vector of the new 3 Trapezoids.
-     * The first one will replace the one that is being replaced */
-    size_t t1Index = index;
+     * The first one will replace the one that is being splitted */
+    size_t t1Index = trpzToReplace;
     size_t t2Index = addTrapezoid(t2);
     size_t t3Index = addTrapezoid(t3);
 
@@ -87,82 +84,92 @@ void TrapezoidalMap::split3Left(const int index, cg3::Segment2d& segment) {
     /* Updating t2's neighbors */
     t2.setTopLeftNeighbor(t1Index);
     t2.setTopRightNeighbor(origin.getTopRightNeighbor());
-    t2.setBotLeftNeighbor(0);
-    t2.setBotRightNeighbor(0);
 
     /* Updating t3's neighbors */
-    t3.setTopLeftNeighbor(0);
-    t3.setTopRightNeighbor(0);
     t3.setBotLeftNeighbor(t1Index);
     t3.setBotRightNeighbor(origin.getBotRightNeighbor());
 
     /* Updating */
-    updateTrapezoid(index, t1);
+    updateTrapezoid(trpzToReplace, t1);
 }
 
-void TrapezoidalMap::split3Right(const int index, cg3::Segment2d& segment) {
+void TrapezoidalMap::split2MergeTop(const size_t trpzToReplace, const cg3::Segment2d& segment, const size_t trpzToMerge, const size_t trpzNB) {
     /* Reference of the Trapezoid that needs to be splitted */
-    Trapezoid origin = trapezoids[index];
+    Trapezoid origin = trapezoids[trpzToReplace];
+    /* Reference of the Trapezoid that needs to be merged */
+    Trapezoid merge = trapezoids[trpzToMerge];
 
-    /* Creation of the new 3 Trapezoids */
+    /* Creation of the new 2 Trapezoids */
+    Trapezoid t1 = Trapezoid(origin.getTop(), segment, merge.getLeftP(), origin.getRightP());
+    Trapezoid t2 = Trapezoid(segment, origin.getBot(), origin.getLeftP(), origin.getRightP());
+
+    /* Updating t1's neighbors */
+    t1.setTopLeftNeighbor(merge.getTopLeftNeighbor());
+    t1.setBotLeftNeighbor(merge.getBotLeftNeighbor());
+
+    /* Updating t2's neighbors */
+    t2.setTopLeftNeighbor(trpzNB);
+
+    /* Updating the left neighbors' neighbors */
+    trapezoids[trpzNB].setTopRightNeighbor(trpzToReplace);
+
+    /* Updating */
+    updateTrapezoid(trpzToMerge, t1);
+    updateTrapezoid(trpzToReplace, t2);
+}
+
+void TrapezoidalMap::split2MergeBot(const size_t trpzToReplace, const cg3::Segment2d& segment, const size_t trpzToMerge, const size_t trpzNB) {
+    /* Reference of the Trapezoid that needs to be splitted */
+    Trapezoid origin = trapezoids[trpzToReplace];
+    /* Reference of the Trapezoid that needs to be merged */
+    Trapezoid merge = trapezoids[trpzToMerge];
+
+    /* Creation of the new 2 Trapezoids */
+    Trapezoid t1 = Trapezoid(origin.getTop(), segment, origin.getLeftP(), origin.getRightP());
+    Trapezoid t2 = Trapezoid(segment, origin.getBot(), merge.getLeftP(), origin.getRightP());
+
+    /* Updating t1's neighbors */
+    t1.setBotLeftNeighbor(trpzNB);
+
+    /* Updating t2's neighbors */
+    t2.setTopLeftNeighbor(merge.getTopLeftNeighbor());
+    t2.setBotLeftNeighbor(merge.getBotLeftNeighbor());
+
+    /* Updating the left neighbors' neighbors */
+    trapezoids[trpzNB].setTopRightNeighbor(trpzToReplace);
+
+    /* Updating */
+    updateTrapezoid(trpzToReplace, t1);
+    updateTrapezoid(trpzToMerge, t2);
+}
+
+/*
+void TrapezoidalMap::split3Right(const size_t trpzToReplace, const cg3::Segment2d& segment, const size_t trpzToMerge, const size_t trpzNB) {
+    Trapezoid origin = trapezoids[trapezoidToReplaceIndex];
+
     Trapezoid t1 = Trapezoid(origin.getTop(), segment, origin.getLeftP(), segment.p2());
     Trapezoid t2 = Trapezoid(segment, origin.getBot(), origin.getLeftP(), segment.p2());
     Trapezoid t3 = Trapezoid(origin.getTop(), origin.getBot(), segment.p2(), origin.getRightP());
 
-    /* Indexes in the Trapezoids Vector of the new 3 Trapezoids.
-     * The first one will replace the one that is being replaced */
-    size_t t1Index = index;
+    size_t t1Index = trapezoidToReplaceIndex;
     size_t t2Index = addTrapezoid(t2);
     size_t t3Index = addTrapezoid(t3);
 
-    /* Updating t1's neighbors */
     t1.setTopLeftNeighbor(origin.getTopLeftNeighbor());
     t1.setTopRightNeighbor(t3Index);
     t1.setBotLeftNeighbor(0);
     t1.setBotRightNeighbor(0);
 
-    /* Updating t2's neighbors */
     t2.setTopLeftNeighbor(0);
     t2.setTopRightNeighbor(0);
     t2.setBotLeftNeighbor(origin.getBotLeftNeighbor());
     t2.setBotRightNeighbor(t3Index);
 
-    /* Updating t1's neighbors */
     t3.setTopLeftNeighbor(t1Index);
     t3.setBotLeftNeighbor(t2Index);
     t3.setTopRightNeighbor(origin.getTopRightNeighbor());
     t3.setBotRightNeighbor(origin.getBotRightNeighbor());
 
-
-    /* Updating */
-    updateTrapezoid(index, t1);
+    updateTrapezoid(trapezoidToReplaceIndex, t1);
 }
-
-void TrapezoidalMap::split2(const int index, cg3::Segment2d& segment) {
-    /* Reference of the Trapezoid that needs to be splitted */
-    Trapezoid origin = trapezoids[index];
-
-    /* Creation of the new 2 Trapezoids */
-    Trapezoid t1 = Trapezoid(origin.getTop(), segment, origin.getLeftP(), origin.getRightP());
-    Trapezoid t2 = Trapezoid(segment, origin.getBot(), origin.getLeftP(), origin.getRightP());
-
-    /* Indexes in the Trapezoids Vector of the new 2 Trapezoids.
-     * The first one will replace the one that is being replaced */
-    size_t t1Index = index;
-    size_t t2Index = addTrapezoid(t2);
-
-    /* Updating t1's neighbors */
-    t1.setTopLeftNeighbor(origin.getTopLeftNeighbor());
-    t1.setTopRightNeighbor(origin.getTopRightNeighbor());
-    t1.setBotLeftNeighbor(0);
-    t1.setBotRightNeighbor(0);
-
-    /* Updating t2's neighbors */
-    t2.setTopLeftNeighbor(0);
-    t2.setTopRightNeighbor(0);
-    t2.setBotLeftNeighbor(origin.getBotLeftNeighbor());
-    t2.setBotRightNeighbor(origin.getBotRightNeighbor());
-
-    /* Updating */
-    updateTrapezoid(index, t1);
-}
+*/
--- data_structures/trapezoidalmap.h
+++ data_structures/trapezoidalmap.h
@@ -6,17 +6,22 @@
 class TrapezoidalMap {
     private:
         std::vector<Trapezoid> trapezoids;
+
+        void updateTrapezoid(const size_t index, const Trapezoid& trapezoid);
+
+        void split4(const size_t trpzToReplace, const cg3::Segment2d& segment);
+        void split3Left(const size_t trpzToReplace, const cg3::Segment2d& segment);
+        void split2MergeTop(const size_t trpzToReplace, const cg3::Segment2d& segment, const size_t trpzToMerge, const size_t trpzNB);
+        void split2MergeBot(const size_t trpzToReplace, const cg3::Segment2d& segment, const size_t trpzToMerge, const size_t trpzNB);
+        void split3RightMergeTop(const size_t trpzToReplace, const cg3::Segment2d& segment, const size_t trpzToMerge, const size_t trpzNB);
+        void split3RightMergeBot(const size_t trpzToReplace, const cg3::Segment2d& segment, const size_t trpezToMerge, const size_t trpzNB);
     public:
         TrapezoidalMap();
         ~TrapezoidalMap();
 
         size_t addTrapezoid(const Trapezoid& trapezoid);
-        void updateTrapezoid(const int index, const Trapezoid& trapezoid);
 
-        void split4(const int index, cg3::Segment2d& segment);
-        void split3Left(const int index, cg3::Segment2d& segment);
-        void split3Right(const int index, cg3::Segment2d& segment);
-        void split2(const int index, cg3::Segment2d& segment);
+        void updateTrapezoidalMap(const size_t startTrpz, const cg3::Segment2d& segment);
 };
 
 #endif // TRAPEZOIDALMAP_H
