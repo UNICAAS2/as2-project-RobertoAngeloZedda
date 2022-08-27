cbd90e33b70caa9e22cced9ef3a8ae332596da40
Roberto <xrhayux@gmail.com>
2022-08-27 17:36:05 +0200

    some computational weight moved from the draw function to the map creation

---
+++ drawables/drawabletrapezoid.cpp
@@ -0,0 +1,6 @@
+#include "drawabletrapezoid.h"
+
+DrawableTrapezoid::DrawableTrapezoid()
+{
+
+}
---
+++ drawables/drawabletrapezoid.h
@@ -0,0 +1,11 @@
+#ifndef DRAWABLETRAPEZOID_H
+#define DRAWABLETRAPEZOID_H
+
+
+class DrawableTrapezoid
+{
+public:
+    DrawableTrapezoid();
+};
+
+#endif // DRAWABLETRAPEZOID_H
